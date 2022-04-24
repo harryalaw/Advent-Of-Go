96a76c66f57187953bd7efa1d7f6a74bfff5790b
harryalaw <harry@Harrys-MacBook-Pro.local>
2022-04-24 18:25:29 +0100

    Add day 4

---
+++ 2015/04/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func getMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
+}
+
+func solveHash(prefix, input string) int {
+	for i := 1; ; i++ {
+		testString := fmt.Sprintf("%s%d", input, i)
+		hash := getMD5Hash(testString)
+		if strings.HasPrefix(hash, prefix) {
+			return i
+		}
+	}
+}
+
+func part1(input string) int {
+	return solveHash("00000", input)
+}
+
+func part2(input string) int {
+	return solveHash("000000", input)
+}
+
+func parseInput() string {
+	content, err := ioutil.ReadFile("data.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
+}
+
+func solve(part func(string) int) int {
+	return part(parseInput())
+}
+
+func test(part func(string) int, input string, expected int) bool {
+	result := part(input)
+	return result == expected
+}
+
+func runTests(actual bool) bool {
+	test1 := test(part1, "abcdef", 609043)
+	test2 := test(part1, "pqrstuv", 1048970)
+
+	test3 := true
+	test4 := true
+	if actual {
+		test3 = test(part1, parseInput(), 254575)
+		test4 = test(part2, parseInput(), 1038736)
+	}
+	return test1 && test2 && test3 && test4
+}
+
+func main() {
+	println(runTests(false))
+	println(solve(part1))
+	println(solve(part2))
+}
