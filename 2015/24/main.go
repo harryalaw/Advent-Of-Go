47343efc70da334421851cd48e1d4fef887420e3
harryalaw <harry@Harrys-MacBook-Pro.local>
2022-06-25 12:51:16 +0100

    Optimize search for partitions

--- 2015/24/main.go
+++ 2015/24/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"sort"
 	"strings"
 )
 
@@ -43,7 +44,7 @@ func parseInput(fileName string) []int {
 	return numbers
 }
 
-// find all subsets of numbers that sum to K
+// find all subsets of an int array that sum to K
 func sumToK(numbers []int, k int) [][]int {
 	out := make([][]int, 0)
 
@@ -58,7 +59,11 @@ func sumToK(numbers []int, k int) [][]int {
 func recursiveSumToK(numbers []int, attempt []int, k int, out [][]int) [][]int {
 	total := sumArray(attempt)
 	if total == k {
-		out = append(out, attempt)
+		tempAttempt := copyInts(attempt)
+		out = append(out, tempAttempt)
+		return out
+	}
+	if len(numbers) == 0 {
 		return out
 	}
 
@@ -66,6 +71,39 @@ func recursiveSumToK(numbers []int, attempt []int, k int, out [][]int) [][]int {
 		if total+number <= k {
 			tempAttempt := append(attempt, number)
 			out = recursiveSumToK(numbers[i+1:], tempAttempt, k, out)
+		}
+	}
+	return out
+}
+
+func sumToKPruned(numbers []int, k int) [][]int {
+	out := make([][]int, 0)
+
+	minlength := len(numbers)
+	for i, number := range numbers {
+		attempt := []int{number}
+		out = recursiveSumToKPruned(numbers[i+1:], attempt, k, out, &minlength)
+	}
+	return out
+}
+
+func recursiveSumToKPruned(numbers, attempt []int, k int, out [][]int, minLength *int) [][]int {
+	total := sumArray(attempt)
+	if total == k && len(attempt) <= *minLength {
+		if len(attempt) < *minLength {
+			out = make([][]int, 0)
+			*minLength = len(attempt)
+		}
+
+		tempAttempt := copyInts(attempt)
+		out = append(out, tempAttempt)
+		return out
+	}
+
+	for i, number := range numbers {
+		if total+number <= k && len(attempt) < *minLength {
+			tempAttempt := append(attempt, number)
+			out = recursiveSumToKPruned(numbers[i+1:], tempAttempt, k, out, minLength)
 		}
 	}
 	return out
@@ -92,18 +130,18 @@ func multiplyArray(array []int) int {
 }
 
 func part1(numbers []int) int {
+	numbers = sortDescending(numbers)
 	target := sumArray(numbers)
 	if target%3 != 0 {
 		panic("Not divisible by 3")
 	}
 	target /= 3
 
-	partitions := sumToK(numbers, target)
+	partitions := sumToKPruned(numbers, target)
 
 	minLength := len(numbers)
 	quantumEntanglement := math.MaxInt64
-	for i := len(partitions) - 1; i >= 0; i-- {
-		sum := partitions[i]
+	for _, sum := range partitions {
 		if len(sum) > minLength {
 			continue
 		}
@@ -118,27 +156,56 @@ func part1(numbers []int) int {
 }
 
 func part2(numbers []int) int {
+	numbers = sortDescending(numbers)
 	target := sumArray(numbers)
 	if target%4 != 0 {
 		panic("Not divisible by 4")
 	}
 	target /= 4
 
-	partitions := sumToK(numbers, target)
+	partitions := sumToKPruned(numbers, target)
 
 	minLength := len(numbers)
 	quantumEntanglement := math.MaxInt64
+	var finalPartition []int
+
 	for i := len(partitions) - 1; i >= 0; i-- {
 		sum := partitions[i]
-		if len(sum) > minLength {
+		if len(sum) > minLength || sumArray(sum) != target {
 			continue
 		}
 		tempQE := multiplyArray(sum)
 		if tempQE < quantumEntanglement || len(sum) < minLength {
 			quantumEntanglement = tempQE
+			finalPartition = sum
 		}
 
-		minLength = len(sum)
+		if len(sum) < minLength {
+			minLength = len(sum)
+		}
 	}
+	fmt.Printf("Final Partition: %+v\n", finalPartition)
 	return quantumEntanglement
+}
+
+func sortDescending(numbers []int) []int {
+	tempNumbers := numbers
+	sort.Ints(tempNumbers)
+
+	sorted := make([]int, len(tempNumbers))
+	for i := 0; i < len(tempNumbers); i++ {
+		sorted[len(tempNumbers)-i-1] = tempNumbers[i]
+	}
+
+	return sorted
+}
+
+func copyInts(numbers []int) []int {
+	out := make([]int, len(numbers))
+
+	for i, number := range numbers {
+		out[i] = number
+	}
+
+	return out
 }
--- 2015/24/main_test.go
+++ 2015/24/main_test.go
@@ -5,6 +5,38 @@ import (
 	"testing"
 )
 
+func Test_sortDescending(t *testing.T) {
+	tests := []struct {
+		name string
+		args []int
+		want []int
+	}{
+		{
+			name: "Descending list stays same",
+			args: []int{4, 3, 2, 1},
+			want: []int{4, 3, 2, 1},
+		},
+		{
+			name: "Scrambled array becomes sorted",
+			args: []int{1, 3, 2, 4},
+			want: []int{4, 3, 2, 1},
+		},
+		{
+			name: "Sorted ascending becomes descending",
+			args: []int{1, 2, 3, 4},
+			want: []int{4, 3, 2, 1},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortDescending(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortDescending() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
 func Test_part1(t *testing.T) {
 	type args struct {
 		numbers []int
@@ -97,12 +129,113 @@ func Test_sumToK(t *testing.T) {
 				{2, 8}, {3, 7}, {4, 6}, {10},
 			},
 		},
+		{
+			name: "Longer sum after valid sum",
+			args: args{
+				numbers: []int{9, 3, 2, 1},
+				k:       12,
+			},
+			want: [][]int{
+				{9, 3}, {9, 2, 1},
+			},
+		},
+		{
+			name: "Descending values that don't sum to target",
+			args: args{
+				numbers: []int{5, 4, 3, 2, 1},
+				k:       20,
+			},
+			want: [][]int{},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := sumToK(tt.args.numbers, tt.args.k); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("sumToK() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_sumToKPruned(t *testing.T) {
+	type args struct {
+		numbers []int
+		k       int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "1-5 sum to 4",
+			args: args{
+				numbers: []int{1, 2, 3, 4, 5},
+				k:       4,
+			},
+			want: [][]int{{4}},
+		},
+		{
+			name: "1-5 sum to 15",
+			args: args{
+				numbers: []int{1, 2, 3, 4, 5},
+				k:       15,
+			},
+			want: [][]int{{1, 2, 3, 4, 5}},
+		},
+		{
+			name: "1-10 sum to 10",
+			args: args{
+				numbers: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				k:       10,
+			},
+			want: [][]int{{10}},
+		},
+		{
+			name: "Longer sum after valid sum",
+			args: args{
+				numbers: []int{9, 3, 2, 1},
+				k:       12,
+			},
+			want: [][]int{{9, 3}},
+		},
+		{
+			name: "Descending values that don't sum to target",
+			args: args{
+				numbers: []int{5, 4, 3, 2, 1},
+				k:       20,
+			},
+			want: [][]int{},
+		},
+		{
+			name: "Example from page",
+			args: args{
+				numbers: []int{1, 2, 3, 4, 5, 7, 8, 9, 10, 11},
+				k:       15,
+			},
+			want: [][]int{{4, 11}, {5, 10}, {7, 8}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumToKPruned(tt.args.numbers, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sumToKPruned() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_doPart2(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			doPart2()
 		})
 	}
 }
