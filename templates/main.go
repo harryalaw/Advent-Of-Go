142546581992dd69e2d0a0b2e6699314635d2e14
harryalaw <harryalaw@gmail.com>
2022-12-01 06:57:36 +0000

    Add more details to template files

--- templates/main.go
+++ templates/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 
 	util "github.com/harryalaw/advent-of-go/util"
 )
@@ -10,15 +11,29 @@ import (
 //go:embed data.txt
 var input string
 
+func parseInput(input string) []string {
+	return strings.Split(strings.TrimSpace(input), "\n")
+}
+
 func main() {
 	util.Time(doPart1, "Part 1")
 	util.Time(doPart2, "Part 2")
 }
 
 func doPart1() {
-	fmt.Println("")
+	data := parseInput(input)
+	fmt.Println("Part 1: ", Part1(data))
 }
 
 func doPart2() {
-	fmt.Println("")
+	data := parseInput(input)
+	fmt.Println("Part 2: ", Part2(data))
+}
+
+func Part1(data []string) int {
+	return -1
+}
+
+func Part2(data []string) int {
+	return -1
 }
--- templates/main_test.go
+++ templates/main_test.go
@@ -1,8 +1,37 @@
 package main
 
-import "testing"
+import (
+	_ "embed"
+	"testing"
+)
 
-func Test(t *testing.T) {
+//go:embed test_data.txt
+var testData string
 
-	t.Errorf("No test written")
+func TestParseInput(t *testing.T) {
+	expected := []string{}
+
+	parsedInput := parseInput(testData)
+
+	if len(parsedInput) != len(expected) {
+		t.Errorf("Did not create %d rows, got=%d", len(expected), len(parsedInput))
+	}
+
+	t.Errorf("Not asserting on contents of parsed input")
+}
+
+func TestPart1(t *testing.T) {
+	input := parseInput(testData)
+
+	value := Part1(input)
+
+	t.Errorf("Not asserting on result of Part 1")
+}
+
+func TestPart2(t *testing.T) {
+	input := parseInput(testData)
+
+	value := Part2(input)
+
+	t.Errorf("Not asserting on result of Part 1")
 }
