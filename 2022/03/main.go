42ccdd0480b4973449947a4899d879313c3fe428
harryalaw <harryalaw@gmail.com>
2022-12-03 06:42:24 +0000

    Speed up by ~8x
    - Replace map with rune arithmetic
    - Replace arrays with integer totals

--- 2022/03/main.go
+++ 2022/03/main.go
@@ -31,24 +31,22 @@ func doPart2() {
 }
 
 func Part1(data []string) int {
-	commonParts := make([]int, len(data))
+	total := 0
 
-	for i, line := range data {
+	for _, line := range data {
 		length := len(line)
 		firstHalf := line[0 : length/2]
 		secondHalf := line[length/2:]
 		firstLetters := getLetters(firstHalf)
 		secondLetters := getLetters(secondHalf)
-		commonParts[i] = findCommonLetter(firstLetters, secondLetters)
+
+		total += findCommonLetter(firstLetters, secondLetters) + 1
 	}
 
-	return util.IntSum(commonParts...)
+	return total
 }
 
-func findCommonLetter(a, b []bool) int {
-	if len(a) != len(b) {
-		panic("lengths aren't equal")
-	}
+func findCommonLetter(a, b [52]bool) int {
 	for i, value := range a {
 		if value && b[i] {
 			return i
@@ -58,11 +56,8 @@ func findCommonLetter(a, b []bool) int {
 	panic("No match")
 }
 
-func findCommonLetters(a, b []bool) []bool {
-	if len(a) != len(b) {
-		panic("lengths aren't equal")
-	}
-	out := make([]bool, 53)
+func findCommonLetters(a, b [52]bool) [52]bool {
+	out := [52]bool{}
 	for i, value := range a {
 		if value && b[i] {
 			out[i] = true
@@ -71,79 +66,29 @@ func findCommonLetters(a, b []bool) []bool {
 	return out
 }
 
-func getLetters(item string) []bool {
-	out := make([]bool, 53)
-	valueMap := map[rune]int{
-		'a': 1,
-		'b': 2,
-		'c': 3,
-		'd': 4,
-		'e': 5,
-		'f': 6,
-		'g': 7,
-		'h': 8,
-		'i': 9,
-		'j': 10,
-		'k': 11,
-		'l': 12,
-		'm': 13,
-		'n': 14,
-		'o': 15,
-		'p': 16,
-		'q': 17,
-		'r': 18,
-		's': 19,
-		't': 20,
-		'u': 21,
-		'v': 22,
-		'w': 23,
-		'x': 24,
-		'y': 25,
-		'z': 26,
-		'A': 27,
-		'B': 28,
-		'C': 29,
-		'D': 30,
-		'E': 31,
-		'F': 32,
-		'G': 33,
-		'H': 34,
-		'I': 35,
-		'J': 36,
-		'K': 37,
-		'L': 38,
-		'M': 39,
-		'N': 40,
-		'O': 41,
-		'P': 42,
-		'Q': 43,
-		'R': 44,
-		'S': 45,
-		'T': 46,
-		'U': 47,
-		'V': 48,
-		'W': 49,
-		'X': 50,
-		'Y': 51,
-		'Z': 52,
-	}
+func getLetters(item string) [52]bool {
+	out := [52]bool{}
 	for _, char := range item {
-
-		out[valueMap[char]] = true
+		if char >= 97 {
+			out[char-97] = true
+		} else {
+			out[char-65+26] = true
+		}
 	}
 
 	return out
 }
 
 func Part2(data []string) int {
-	commonParts := make([]int, len(data))
+	total := 0
+
 	for i := 0; i < len(data)-2; i += 3 {
 		firstBag := getLetters(data[i])
 		secondBag := getLetters(data[i+1])
 		thirdBag := getLetters(data[i+2])
 
-		commonParts[i] = findCommonLetter(findCommonLetters(firstBag, secondBag), thirdBag)
+		total += findCommonLetter(findCommonLetters(firstBag, secondBag), thirdBag) + 1
 	}
 
-	return util.IntSum(commonParts...)
+	return total
 }
